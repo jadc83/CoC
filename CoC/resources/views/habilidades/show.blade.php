cf90e1cff8b5a55ee69e757b14dfdade5f58ee34
Jose Antonio Dominguez Camacho <joseantonio.dominguez@iesdonana.org>
2024-02-26 21:14:24 +0100

    Update 1

--- CoC/resources/views/habilidades/show.blade.php
+++ CoC/resources/views/habilidades/show.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 
-    <div class="p-4 object-center text-white bg-cyan-700 ">
-        <p class="p-4">{{ $habilidad->habilidad }}</p>
-        <p class="p-4 text-xl">{{ $habilidad->prob }}%</p>
-        <p class="p-4">{{ $habilidad->descripcion }}</p>
+    <div class="w-[110rem] p-4 mt-4 object-center text-white text-center bg-cyan-700 ">
+        <p class="p-4 text-3xl">{{ $habilidad->habilidad }}</p>
+        <p class="p-4 text-3xl">{{ $habilidad->prob }}%</p>
+        <p class="p-4 text-3xl">{{ $habilidad->descripcion }}</p>
     </div>
 
 @endsection
